6b4ee50ca9a50f868e69cf305b54ebd274ee6257
JuanFidelDelgado <delgadojuanfidel@gmail.com>
2023-02-28 17:22:37 -0500

    Ajuste 28/02/2023

--- index.php
+++ index.php
@@ -24,41 +24,55 @@ if (isset($_REQUEST['mensaje'])) $mensaje=$_REQUEST['mensaje'];
     </head>
     <body>
         <div id="index" align="center">
-            <h2>CLÍNICA VETERINARIA ANIMAL CENTER</h2>
+            <h1>CLÍNICA VETERINARIA ANIMAL CENTER</h1>
             <h4>INICIO DE SESIÓN</h4>
             <p><font color='red' ><?= $mensaje ?></font></p>
             <form name="formulario" method="post" action="control/validar.php">
-            <!--<form name="formulario" method="post" action="inicio.php">-->
-            <table border="0">
-                <tr><th>Usuario</th><th><input type="text" name="usuario" required/></th></tr>
-                <tr><th>Contraseña</th><th><input type="password" name="clave" required/></th></tr>
-            </table>
-            <p></p>
-            <input type="submit" value="Ingresar"/>
+                <!--<form name="formulario" method="post" action="inicio.php">-->
+                <table border="0">
+                    <tr width="1000px" border="1px"><th width="auto">Usuario</th><th><input type="text" name="usuario" required/></th></tr>
+                    <tr><th>Contraseña</th><th><input type="password" name="clave" required/></th></tr>
+                </table>
+                <p></p>
+                <input type="submit" value="Ingresar"/>
             </form>
             <br>
             <a href="presentacion/configuracion/registroFormulario.php?accion=Adicionar">Registrarse</a>
+            <br>
             <br>
             <!--
             <a href="presentacion/configuracion/usuariosFormulario.php">Registrarse</a>
             <a href="presentacion/configuracion/usuariosFormulario.php">Cambiar clave</a>
             -->
         </div>
-        <table align="center">
-            <tr>
-                <th><img src="presentacion/imagenes/portada/Clinica1.jpg" height="250"></th>
-                <th><img src="presentacion/imagenes/portada/Clinica2.jpg" height="250"></th>
-                <th><img src="presentacion/imagenes/portada/Clinica3.jpg" height="250"></th>
-            </tr>
-            <tr align="center"><td colspan="3">NUESTROS SERVICIOS DE APOYO</td></tr>
-            <tr align="center"><td colspan="3">Consulta veterinaria presencial, servicio a domicilio, cirugías y hospitalización, laboratorio, radiología y ecografía.</td>            </tr>
-            <tr>
-                <th colspan="3">
+        <center>
+            <table>
+                <tr>
+                    <section class="imagenes">
+                        <img src="presentacion/imagenes/portada/Clinica1.jpg" height="450"> 
+                        <img src="presentacion/imagenes/portada/Clinica2.jpg" height="450">
+                        <img src="presentacion/imagenes/portada/Clinica3.jpg" height="450">
+                    </section>
+                </tr>
+                <tr>
                     <a href="presentacion/configuracion/registroFormulario.php?accion=Adicionar">
-                    <img src="presentacion/imagenes/portada/cita-de-control.webp" height="250">
-                    </a>
-                </th>
-            </tr>
-        </table>
+                    <img src="presentacion/imagenes/portada/cita-de-control.webp"></a>
+                </tr>
+            </table>
+        </center>
+        <div align="center">
+            <h1>NUESTROS SERVICIOS DE APOYO</h1>
+            <br>
+            <h2>Consulta veterinaria presencial, servicio a domicilio, cirugías y hospitalización, laboratorio, radiología y ecografía.</h2>
+        </div>
+    <!--
+        <center>
+            <section class="imagenes1" align="center">
+                <a href="presentacion/configuracion/registroFormulario.php?accion=Adicionar">
+                <img src="presentacion/imagenes/portada/cita-de-control.webp">
+                </a>
+            </section>
+        </center>
+    -->
     </body>
 </html>
